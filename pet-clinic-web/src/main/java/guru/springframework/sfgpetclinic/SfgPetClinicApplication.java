9239b068fdff6c66b4267868770d2915e261d0d6
wilkojc <wilkojc.kacper@gmail.com>
2020-08-07 18:27:33 +0200

    Reconfigured poms. Added test class.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -3,11 +3,12 @@ package guru.springframework.sfgpetclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SfgPetClinicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgPetClinicApplication.class, args);
+		System.out.println("hello");
+	//	SpringApplication.run(SfgPetClinicApplication.class, args);
 	}
 
 }
