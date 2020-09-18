57b6a6051f535be894f907269ff07ef8421c9c38
wilkojc <wilkojc.kacper@gmail.com>
2020-09-18 21:31:15 +0200

    Created Visit, updated Owner with pet set. Closes #27.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.io.Serializable;
 
-public class BaseEntity implements Serializable {
+public abstract class BaseEntity implements Serializable {
 
     private Long id;
 
