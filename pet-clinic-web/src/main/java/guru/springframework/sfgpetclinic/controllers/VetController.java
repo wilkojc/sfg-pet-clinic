4cfc95a4bf7ae5acc56156acca25faef0cb2e90e
wilkojc <wilkojc.kacper@gmail.com>
2020-09-20 17:17:31 +0200

    Fixed menu links. Closes #45.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -15,7 +15,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"/vets.html","/vets", "/vets/index", "/vets/index.html"})
     public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
