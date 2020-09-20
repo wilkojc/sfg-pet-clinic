e4abeb462cbfd933123f63dd8e8ee6a6409ab0df
wilkojc <wilkojc.kacper@gmail.com>
2020-09-20 11:44:11 +0200

    Added contact info to owner. Closes #28.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -3,7 +3,34 @@ package guru.springframework.sfgpetclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+    private String address;
+    private String telephone;
+    private String city;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 
     public Set<Pet> getPets() {
         return pets;
