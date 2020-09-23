204aa717e25a5f6c508141fe3c7ccb9ffd0e0a1a
wilkojc <wilkojc.kacper@gmail.com>
2020-09-23 21:05:45 +0200

    Added springdatajpa services for Pets, PetTypes and Specialties . Closes #42 #43 #44.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,46 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import guru.springframework.sfgpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class SpecialtySDJpaService implements SpecialtyService {
+
+    private SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return specialtyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+    specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+    specialtyRepository.deleteById(id);
+    }
+}
